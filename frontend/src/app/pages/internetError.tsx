79a7e85b77ed6e3d53ad6708013128099fb0b765
Jared Rosas <jaredsbt@gmail.com>
2025-01-27 15:31:33 -0800

    Completed No internet Page

--- frontend/src/app/pages/internetError.tsx
+++ frontend/src/app/pages/internetError.tsx
@@ -2,32 +2,39 @@ import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { Button } from "../components/Button";
+import OrangeMascot from "../../assets/orange-mascot";
 
 const styles = StyleSheet.create({
-  container: {
+  pageContainer: {
     flex: 1,
     backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "center",
   },
+  msgContainer: {
+    // backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 218,
+    marginBottom: 25,
+    gap: 10,
+  },
   noConnection: {
     fontFamily: "Inter",
     fontSize: 32,
-    fontWeight: "600",
+    fontWeight: 700,
     lineHeight: 38.4,
     letterSpacing: -0.02,
     textAlign: "center",
-    textDecorationLine: "underline",
     textDecorationStyle: "solid",
     color: "black",
   },
   noConnectionMessage: {
     fontFamily: "Open Sans",
     fontSize: 17,
-    fontWeight: "400",
+    fontWeight: 400,
     lineHeight: 25.5,
     textAlign: "center",
-    textDecorationLine: "underline",
     textDecorationStyle: "solid",
   },
 });
@@ -36,11 +43,14 @@ export default function InternetError() {
   const [isConnected, setIsConnected] = useState(true);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.noConnection}>No Connection</Text>
-      <Text style={styles.noConnectionMessage}>
-        No internet connection found. Please check your connection or try again.
-      </Text>
+    <View style={styles.pageContainer}>
+      <View style={styles.msgContainer}>
+        <OrangeMascot/>
+        <Text style={styles.noConnection}>No connection</Text>
+        <Text style={styles.noConnectionMessage}>
+          No internet connection found. Please check your connection or try again.
+        </Text>
+      </View>
       <Button
         onClick={() => {
           setIsConnected(!isConnected);
