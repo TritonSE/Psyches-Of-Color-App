f68459b45a8e0a2075c91d4635fb4beceb88e1a3
Rudraksh Bhandari <rudrakshbhandari@Rudrakshs-MacBook-Pro.local>
2025-02-18 17:50:30 -0800

    Commented out two lines from InternetError.tsx so the app would compile and run

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@
 
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 import { userProfileRouter } from "./routes/userProfile";
 
@@ -16,19 +16,19 @@ const MONGODB_URI = process.env.MONGODB_URI ?? "";
 app.use(express.json());
 app.use(userProfileRouter);
 
-// mongoose
-//   .connect(MONGODB_URI)
-//   .then(() => {
-//     console.log("✅ Connected to MongoDB");
-//   })
-//   .catch((error: unknown) => {
-//     console.error("❌ MongoDB connection error:", error);
-//     process.exit(1);
-//   });
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((error: unknown) => {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
+  });
 
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("Express + TypeScript Server");
-// });
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
