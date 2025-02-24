800d80c1f3175f7c2acf7e15a34767cd89b679d6
Jared Rosas <jaredsbt@gmail.com>
2025-02-23 20:40:51 -0800

    created editProile file

---
+++ frontend/src/app/pages/editProfile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  hi: {
+    flex: 1,
+  },
+});
+
+export default function editProfile() {
+  return (
+    <View>
+      <Text>Edit Profile</Text>
+    </View>
+  );
+}
