9231ff60df572419308b6079ac25a5c2bd5ad8fb
alexisvvega <a9vega@ucsd.edu>
2025-03-04 15:54:18 -0800

    error 65

--- frontend/src/app/settingsPage.tsx
+++ frontend/src/app/settingsPage.tsx
@@ -1,7 +1,8 @@
-//import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { useState } from "react";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -12,13 +13,13 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-//import { lightModeColors } from "@/constants/colors";
 
 export default function SettingsScreen() {
   const router = useRouter();
   const [dailyReminder, setDailyReminder] = useState(false);
   const [locationTracking, setLocationTracking] = useState(false);
   const [reminderTime] = useState("9:00 AM");
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   // Navigation functions
   const navigateToProfilePage = () => {
@@ -28,103 +29,36 @@ export default function SettingsScreen() {
   const navigateToRandomPage = () => {
     router.push("/randomPage");
   };
+  const handleLocationToggle = async () => {
+    if (!locationTracking) {
+      // Request location permissions
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      console.log("status:", status);
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied.");
+        Alert.alert(
+          "Location Permission Denied",
+          "Please enable location permissions in your device settings.",
+        );
+        return;
+      }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#FFFFFF",
-    },
-    scrollContainer: {
-      paddingHorizontal: 20,
-      paddingTop: 70,
-      paddingBottom: 176,
-    },
-    header: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      height: 50,
-    },
-    headerTitle: {
-      fontSize: 18,
-      fontWeight: "600",
-      textAlign: "center",
-      flex: 1,
-      marginRight: 24,
-    },
-    textContainer: {
-      flexShrink: 1,
-      marginRight: 10,
-    },
-    sectionTitle: {
-      color: "#484848",
-      fontFamily: "Social Gothic",
-      fontSize: 18,
-      fontStyle: "normal",
-      fontWeight: "600",
-      lineHeight: 24,
-      letterSpacing: 0.15,
-      marginTop: 40,
-    },
-    card: {
-      backgroundColor: "white",
-      borderRadius: 12,
-      padding: 16,
-      borderWidth: 1,
-      borderColor: "#EBEBEB",
-      marginTop: 10,
-    },
-    topRoundedCard: {
-      borderTopLeftRadius: 12,
-      borderTopRightRadius: 12,
-      borderBottomLeftRadius: 0,
-      borderBottomRightRadius: 0,
-      borderBottomWidth: 1,
-      borderBottomColor: "#E0E0E0",
-    },
-    bottomRoundedCard: {
-      borderTopLeftRadius: 0,
-      borderTopRightRadius: 0,
-      borderBottomLeftRadius: 12,
-      borderBottomRightRadius: 12,
-      marginTop: -1,
-      borderTopWidth: 1,
-      borderTopColor: "#E0E0E0",
-    },
-    row: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      paddingVertical: 10,
-    },
-    cardTitle: {
-      color: "#000",
-      fontFamily: "Archivo",
-      fontSize: 16,
-      fontStyle: "normal",
-      fontWeight: "500",
-      lineHeight: 24,
-      letterSpacing: 0.15,
-    },
-    cardSubtitle: {
-      color: "#6C6C6C",
-      fontFamily: "Archivo",
-      fontSize: 14,
-      fontStyle: "normal",
-      fontWeight: "400",
-      lineHeight: 20,
-      letterSpacing: 0.25,
-    },
-    time: {
-      color: "#000000",
-      fontSize: 14,
-      fontWeight: "400",
-      fontFamily: "Archivo",
-      fontStyle: "normal",
-      lineHeight: 24,
-      letterSpacing: 0.15,
-    },
-  });
+      // Start location tracking
+      try {
+        const location = await Location.getCurrentPositionAsync({});
+        console.log("Location:", location);
+        Alert.alert("Location Enabled", "Location tracking is now active.");
+        setLocationTracking(true);
+      } catch (error) {
+        console.error("Error starting location tracking:", error);
+        setErrorMsg("Failed to start location tracking.");
+      }
+    } else {
+      // Stop location tracking
+      setLocationTracking(false);
+      Alert.alert("Location Disabled", "Location tracking has been turned off.");
+    }
+  };
 
   return (
     <KeyboardAvoidingView
@@ -150,11 +84,10 @@ export default function SettingsScreen() {
             </View>
             <Switch
               value={dailyReminder}
-              onValueChange={() => {
-                setDailyReminder(!dailyReminder);
-              }}
+              onValueChange={() => setDailyReminder(!dailyReminder)}
               trackColor={{ false: "#E5E7EB", true: "#c13d2f" }}
               thumbColor={dailyReminder ? "#FFFFFF" : "#B4B4B4"}
+              style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
             />
           </View>
         </View>
@@ -178,11 +111,10 @@ export default function SettingsScreen() {
             </View>
             <Switch
               value={locationTracking}
-              onValueChange={() => {
-                setLocationTracking(!locationTracking);
-              }}
+              onValueChange={handleLocationToggle}
               trackColor={{ false: "#E5E7EB", true: "#c13d2f" }}
               thumbColor={locationTracking ? "#FFFFFF" : "#B4B4B4"}
+              style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
             />
           </View>
         </View>
@@ -206,3 +138,99 @@ export default function SettingsScreen() {
     </KeyboardAvoidingView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  scrollContainer: {
+    paddingHorizontal: 20,
+    paddingTop: 70,
+    paddingBottom: 176,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 50,
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
+    flex: 1,
+    marginRight: 24,
+  },
+  textContainer: {
+    flexShrink: 1,
+    marginRight: 10,
+  },
+  sectionTitle: {
+    color: "#484848",
+    fontFamily: "Social Gothic",
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    marginTop: 40,
+  },
+  card: {
+    backgroundColor: "white",
+    borderRadius: 12,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#EBEBEB",
+    marginTop: 10,
+  },
+  topRoundedCard: {
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E0E0E0",
+  },
+  bottomRoundedCard: {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+    marginTop: -1,
+    borderTopWidth: 1,
+    borderTopColor: "#E0E0E0",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  cardTitle: {
+    color: "#000",
+    fontFamily: "Archivo",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: 24,
+    letterSpacing: 0.15,
+  },
+  cardSubtitle: {
+    color: "#6C6C6C",
+    fontFamily: "Archivo",
+    fontSize: 14,
+    fontStyle: "normal",
+    fontWeight: "400",
+    lineHeight: 20,
+    letterSpacing: 0.25,
+  },
+  time: {
+    color: "#000000",
+    fontSize: 14,
+    fontWeight: "400",
+    fontFamily: "Archivo",
+    fontStyle: "normal",
+    lineHeight: 24,
+    letterSpacing: 0.15,
+  },
+});
