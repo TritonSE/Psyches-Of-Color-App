ad7241c465efe719310c3caafb0dbb0fbe728f27
vesalcido <vesalcido@ucsd.edu>
2025-04-22 16:14:16 -0700

    Trying to fix merge conflict

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, SafeAreaView, View, Text, Image } from "react-native";
 // import reddue from @/asset;
-import { Button } from 
+//import { Button } from 
 
 const styles = StyleSheet.create({
   page: {
@@ -20,16 +20,40 @@ const styles = StyleSheet.create({
     verticalAlign: "middle",
   },
   textBox: {
-    backgroundColor: "orange",
+    backgroundColor: "#FFC97E80",
   },
+  subtitle: {
+    color: "#6C6C6C", 
+    textAlign: "center",
+    fontFamily: "Poppins",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: 19.2, 
+  },
+  textReg: {
+    color: "#1D1B20",
+    textAlign: "center",
+    fontFamily: "Figtree",
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 27, 
+    letterSpacing: 0.15,
+  }
 });
 
 export default function InternetError() {
   return (
     <SafeAreaView style={styles.page}>
       <View style={styles.titleSection}>
-        <Text style={styles.title}></Text>
+        <Text style={styles.title}> Hey Michael!</Text>
+        <Text style={styles.subtitle}> Welcome Back </Text>
         {/* <Button style={styles.button}></Button> */}
+      </View>
+      <View style={styles.textBox}>
+        <Text style={styles.textReg}> "Just like the seasons change, so do my feelings. This moment is temporary, and I will feel light again."</Text>
+        {/* <Image source={wateringcan.png}></Image> */}
       </View>
       <View style={styles.textBox}>{/* <Image source = {redDude} style ={styles.image} */}</View>
     </SafeAreaView>
