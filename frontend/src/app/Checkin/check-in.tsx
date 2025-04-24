c75559b1cc3f932640307865e448228a9aa2cc02
Rudraksh Bhandari <rudrakshbhandari@Rudrakshs-MacBook-Pro.local>
2025-04-23 18:08:59 -0700

    removed grey bg in header

--- frontend/src/app/Checkin/check-in.tsx
+++ frontend/src/app/Checkin/check-in.tsx
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     position: "relative",
     paddingHorizontal: 20,
     paddingTop: 40,
-    marginBottom: 10,
+    backgroundColor: lightModeColors.background,
   },
   headerTitle: {
     display: "flex",
