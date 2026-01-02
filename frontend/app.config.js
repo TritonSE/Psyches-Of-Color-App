fd10c1451592ffb4cfa9972c5ad18040f16714ee
Benjamin Johnson <ben@skillmapper.com>
2026-01-02 13:37:55 -0800

    use env variable for google services for expo build

---
+++ frontend/app.config.js
@@ -0,0 +1,101 @@
+export default {
+  expo: {
+    name: "frontend",
+    slug: "psyches-of-color-app",
+    scheme: "frontend",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./src/assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./src/assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    fonts: [
+      {
+        asset: "./src/assets/fonts/socialGothic",
+        family: "Social-Gothic",
+      },
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "psychesofcolorapp",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription: "We need your location to provide better services.",
+        NSLocationAlwaysUsageDescription:
+          "We need your location to provide location-based recommendations.",
+        NSCameraUsageDescription: "This app uses the camera to let you take photos for uploading.",
+        NSPhotoLibraryUsageDescription: "This app accesses your photos to let you share them.",
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.psychesofcolor.mobileapp",
+      googleServicesFile: "./google-services.json",
+      permissions: [
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.RECORD_AUDIO",
+      ],
+    },
+    web: {
+      favicon: "./src/assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "2b27ff30-57bf-41ab-8556-e5ebc0e80d2e",
+      },
+      router: {
+        origin: false,
+      },
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
+      [
+        "expo-image-picker",
+        {
+          photosPermission: "The app accesses your photos to let you share them.",
+          cameraPermission: "The app uses the camera to let you take photos for uploading.",
+        },
+      ],
+      [
+        "expo-font",
+        {
+          fonts: [
+            "./src/assets/fonts/Social-Gothic-Regular.otf",
+            "./src/assets/fonts/Social-Gothic-Bold.otf",
+            "./src/assets/fonts/Social-Gothic-DemiBold.otf",
+            "./src/assets/fonts/Social-Gothic-Medium.otf",
+            "./src/assets/fonts/Social-Gothic-Rough.otf",
+            "./src/assets/fonts/Social-Gothic-Soft.otf",
+            "./src/assets/fonts/Social-Gothic-Stencil.otf",
+          ],
+        },
+      ],
+      "expo-router",
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth",
+      "@react-native-google-signin/google-signin",
+    ],
+    owner: "ben332004",
+  },
+};
